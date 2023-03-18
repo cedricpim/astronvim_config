f64292b4445b7d769cbaa1cebf91687a8a5cf0d0
Cedric Pimenta <cedricpim@users.noreply.github.com>
2023-03-18 14:52:04 +0000

    Improve import of plugin Treesj (splitjoin)

--- plugins/splitjoin.lua
+++ plugins/splitjoin.lua
@@ -2,7 +2,5 @@ return {
   'Wansmer/treesj',
   keys = { '<space>v', '<space>j', '<space>s' },
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  config = function()
-    require('treesj').setup({})
-  end,
+  config = function() require('treesj').setup() end,
 }
