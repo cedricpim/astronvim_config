6cbaeae7e80d4619771f147fd4fdd022d58c3006
Cedric Pimenta <cedricpim@users.noreply.github.com>
2023-03-19 00:30:09 +0000

    Cleanup comments

--- plugins/user.lua
+++ plugins/user.lua
@@ -16,14 +16,4 @@ return {
       vim.g.vimwiki_list = { { path = vim.env.VIM_WIKI, syntax = 'markdown', ext = '.md' } }
     end
   },
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
 }
