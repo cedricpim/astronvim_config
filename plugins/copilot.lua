1af56e20ac2aff3c82e077fa06168be3b03c533a
Cedric Pimenta <cedricpim@users.noreply.github.com>
2023-11-17 23:18:05 +0000

    Add copilot

---
+++ plugins/copilot.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "zbirenbaum/copilot-cmp",
+      name = "copilot_cmp",
+      dependencies = { "zbirenbaum/copilot.lua" },
+      opts = {},
+      config = function ()
+       require("copilot_cmp").setup()
+      end
+    },
+    opts = function(_, opts) table.insert(opts.sources, 1, { name = "copilot", priority = 1500 }) end,
+  },
+}
